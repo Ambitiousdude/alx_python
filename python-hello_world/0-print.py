b2c565c1dc813e73474ef32bfbddd424e300fadd
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-07-18 22:50:44 +0000

    0-Hello, print

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,3 +1,2 @@
-# A script that prints a sentence with a new line
 #!/usr/bin/python3
-print('"Programming is like building a multilingual puzzle\n')
+print('"Programming is like building a multilingual puzzle')

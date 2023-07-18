0a8790a74138d5e45a4379694689fcc51838237c
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-07-18 23:12:33 +0000

    1. Copy - Cut - Paste

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = "Hol"
+word_last_2 = "on"
+middle_word = "olberto"
+print("Hol: {}".format(word_first_3))
+print("on: {}".format(word_last_2))
+print("olberto: {}".format(middle_word))

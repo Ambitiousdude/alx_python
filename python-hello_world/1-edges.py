1c91e0ba0c0024e68a3bfa8da75abcc4d1e40cfd
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-07-18 23:17:32 +0000

    Printing using a function

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 word_first_3 = "Hol"
 word_last_2 = "on"
 middle_word = "olberto"
-print("Hol: {}".format(word_first_3))
-print("on: {}".format(word_last_2))
-print("olberto: {}".format(middle_word))
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))

a5c1dc152149a358df7648ff936a5b2666084085
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-07-20 12:24:38 +0000

    Positive anything is better than negative nothing

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -5,6 +5,6 @@ for number in range(-10, 10):
    if number > 0:
       print("{} is postive" .format(number))
    elif number == 0:
-      print("{} is postive" .format(number))
+      print("{} is Zero" .format(number))
    else:
       print("{} is negative" .format(number), end="\n")

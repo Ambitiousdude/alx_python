c2444056d2538c1ce05ce627eb3bd1b3a89b1428
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-07-29 00:33:03 +0000

    Prime Number Function

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def is_prime (number):
     number = int(input("Enter number: "))
-    if number > 1:
-        for i in range (2, number):
-            if (number % i) == 0:
-                return False
-            
-    else:
+    if number <= 1:
+        return False
+    elif number == 2:
         return True
+
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+
+    return True

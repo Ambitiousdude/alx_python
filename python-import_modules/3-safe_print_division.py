9d78265e904532ef05eaa3ce5cac1de634a8b98c
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-07-31 18:49:08 +0000

    Integers division with debug

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# function definition
+def safe_print_division(a, b):
+# using try to test the code for errors 
+    try:
+        result = int(a / b)
+        
+        return result
+# handles the errors
+    except:
+        print("An exception has occured")
+# execute the code regardless of the result of the try, and except
+    finally:
+       print("{}" .format(result))
+    return None
+

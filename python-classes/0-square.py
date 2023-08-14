623e170d1c3364e389e2142636dc364a4c52541c
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-08-14 14:20:38 +0000

    A class Square that defines a square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -8,16 +8,17 @@ Instantiation with size (no type/value verification)
 You are not allowed to import any module
 """
 
-
 class Square:
     """
-        The class is a prototype or blueprint of creating objects. 
-        The class houses the attributes/variables, method/functions
-    """  
-def __init__ (self, size):
+    Represents a square with a private instance attribute 'size'.
+
+    Attributes:
+        size (int): The size of the square's sides.
+
     """
-        A private variable/attribute housing the value of the size.
-        
-    """ 
-    self.__Mysize = size
+    def __init__(self, size):
+        """
+        Private attributes/variables that keeps the size private
+        """
+        self.Mysize= size
     

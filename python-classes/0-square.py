c45540458b372437de63f60f19c7baf31334f378
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-08-14 16:10:08 +0000

    A class Square that defines a square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
 
 
-class Square:
-    """A square class with a private object attribute"""
+"""
+Write a class Square that defines a square by:
 
-    def __init__(self, size):
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
+    You are not allowed to import any module
+"""
+
+
+class square:
+    """
+    The class houses the attributes, and the methods/functions.
+
+    Attributes:
+        size(int): Which takes the arguments in integer.
+    """
+
+    def __init__ (self, size):
+        """
+        The __init__ method is part of the blueprint which takes an argument.
+        Args:
+            size(int): Which takes the arguments in integer.
+
+        """
         self.__size = size

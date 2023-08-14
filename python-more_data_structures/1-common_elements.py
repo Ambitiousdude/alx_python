757073c2d8740ef5d109f8d7019b85cb3362d7a2
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-08-14 15:57:42 +0000

    a class Square that defines a square

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -2,3 +2,12 @@
 
 def common_elements(set1, set2):
     return set1.intersection(set2)
+
+# Or 
+#!/usr/bin/python3
+#def common_elements(set_1, set_2):
+ #   common_1 = set()
+  #  for element in set_1:
+  #      if element in set_2:
+  #          common_1.add(element)
+   # return common_1

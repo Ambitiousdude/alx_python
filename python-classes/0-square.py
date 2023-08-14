da3a8ccc8e07a031216bd9c7091ad43c1b6b4683
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-08-14 15:58:37 +0000

    a class Square that defines a square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-
-
-"""
-Write a class Square that defines a square by:
-
-    Private instance attribute: size
-    Instantiation with size (no type/value verification)
-    You are not allowed to import any module
-"""
+"""Defines a class Square"""
 
 
 class Square:
-   
+    """A square class with a private object attribute"""
+
     def __init__(self, size):
-        self._Square__size = size
+        self.__size = size

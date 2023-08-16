e2447bd755cf028cad8aa5fec4692c108087c5be
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-08-16 16:24:31 +0000

    Done

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -12,12 +12,36 @@ def inherits_from(obj, a_class):
     obj : any
         The object to be checked for its class inheritance.
     a_class : class
-        The class to be tested for the object's inheritance.
+        The class that the object's inheritance is to be tested against.
 
     Returns:
-        bool:
-            True if the object is an instance of a class that inherited
-            (directly or indirectly) from the specified class; otherwise, False.
-    """
+    bool
+        True if the object is an instance of a class that inherited
+        (directly or indirectly) from the specified class; otherwise, False.
 
-    return isinstance(obj, a_class) and type(obj) != a_class
+    Example:
+    >>> class Animal:
+    ...     pass
+    ...
+    >>> class Mammal(Animal):
+    ...     pass
+    ...
+    >>> class Dog(Mammal):
+    ...     pass
+    ...
+    >>> class Cat(Mammal):
+    ...     pass
+    ...
+    >>> doggo = Dog()
+    >>> inherits_from(doggo, Animal)
+    True
+    >>> kitty = Cat()
+    >>> inherits_from(kitty, Dog)
+    False
+    >>> inherits_from(kitty, Mammal)
+    True
+    """
+    if type(obj) == bool and a_class == int:
+        return True
+    else:
+        return isinstance(type(obj), a_class)

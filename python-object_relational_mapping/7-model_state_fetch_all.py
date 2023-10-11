481854c94b226c6a053b2c734b7d4bc758196fc9
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-10-11 10:56:32 +0000

    A script that lists all State objects from the database hbtn_0e_6_usa

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,27 @@
-'''
-script that lists all state objects from the database hbtn_0e_6_usa
-'''
-from model_state import Base, State
+#!/usr/bin/python3
+"""
+This script lists all State objects
+from the database `hbtn_0e_6_usa`.
+"""
+
+from sys import argv
+from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sys
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    # create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+    # create a Session
+    session = Session()
     Base.metadata.create_all(engine)
 
-    session_class = sessionmaker(bind=engine)
-    session = session_class()
-
-    for state in session.query(State).order_by(State.id):
+    s_tate = session.query(State).order_by(State.id).all()
+    for state in s_tate:
         print("{}: {}".format(state.id, state.name))
+    session.close()
